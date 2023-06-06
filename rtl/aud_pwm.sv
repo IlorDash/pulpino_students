50caa810c257f070aaa4e855dcaf7148b3fcf191
jstalex <90794457+jstalex@users.noreply.github.com>
2023-06-06 12:18:58 +0300

    add divider & nokia music

--- rtl/aud_pwm.sv
+++ rtl/aud_pwm.sv
@@ -1,10 +1,21 @@
 `timescale 1ns / 1ps
-module audpwm (
+module top (
     input logic clk,
     output logic aud_pwm,
     input logic start,
-    input logic rst_i
+    input logic resetn
 );
+
+
+logic clkd;
+
+divider d (
+    .clkin(clk),
+    .clkout(clkd),
+    .rst(resetn)
+);
+
+//assign start = SW[0];
 
 parameter DATA_WIDTH = 8;
 
@@ -12,7 +23,7 @@ logic [DATA_WIDTH-1:0] count;
 logic [DATA_WIDTH-1:0] duty;
 
 logic [DATA_WIDTH-1:0] music [0:10000000];
-logic [100:0] index = 0;
+logic [100:0] index;
 
 initial $readmemb("music.mem", music);
     // $readmemb("music.mem", music);
@@ -20,10 +31,10 @@ initial $readmemb("music.mem", music);
 logic [2:0] state;
 
 parameter IDLE = 0, READ = 1, PLAY = 2;
-parameter MUSIC_LEN = 156249;
+parameter MUSIC_LEN = 190000;
 
-always_ff @(posedge clk, posedge start) begin
-    if (rst_i) begin
+always_ff @(posedge clkd, negedge CPU_RESETN) begin
+    if (!resetn) begin
         state = IDLE;
         count = 0;
         index = 0;
