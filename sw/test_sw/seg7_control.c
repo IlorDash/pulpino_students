f65334f02f01bf02989786968d4d6f31cd7f9e19
IlorDash <ilordash02@gmail.com>
2023-06-10 18:30:51 +0000

    driver: seg7: Add 0xE between nums to set emtpy nums, fix reset

--- sw/test_sw/seg7_control.c
+++ sw/test_sw/seg7_control.c
@@ -1,10 +1,12 @@
 #include "seg7_control.h"
 
+#define __riscv__
+
 volatile struct SEG7_CONTROL_APB *seg7_control;
 
 int seg7_control_set_nums(int8_t nums[])
 {
-    int nums_in_octets = 0;
+    int32_t nums_in_octets = 0;
     for (int i = 0; i < NUMS_TO_DISP_NUM; i++)
     {
         if (nums[i] < 0)
@@ -26,7 +28,7 @@ int seg7_control_set_nums(int8_t nums[])
         // Left shift by 24, 12, 0 bits in order to leave empty octets for spaces
         nums_in_octets |= ((((nums[i] / 10) & 0xf) << 4) | ((nums[i] % 10) & 0xf)) << (8 * (NUMS_TO_DISP_NUM - i) - 4 * i);
     }
-
+    nums_in_octets |= 0x00e00e00;
     seg7_control->NUM = nums_in_octets;
     return nums_in_octets;
 }
@@ -35,9 +37,15 @@ void seg7_control_init(void)
 {
     CGREG |= (1 << CGSPI_SEG7_CONTROL);
     seg7_control = (struct SEG7_CONTROL_APB *)(SEG7_CONTROL_BASE_ADDR);
-    seg7_control->RESET = 0;
+    seg7_control->RESET = 0xffffffff;
     for (int i = 0; i < 100; i++)
     {
+        // wait some time
+#ifdef __riscv__
+        asm volatile("nop");
+#else
+        asm volatile("l.nop");
+#endif
     }
-    seg7_control->RESET = 1;
+    seg7_control->RESET = 0;
 }
